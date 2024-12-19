413103155a0be2de6a7de4e8eecc10dc8b42c212
Niki Suckau <niki.suckau@stud.uni-goettingen.de>
2024-12-19 09:25:32 +0100

    Installed new Themes

--- lua/community.lua
+++ lua/community.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
@@ -7,6 +7,14 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  -- colorschemes:
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.colorscheme.tokyodark-nvim" },
+  { import = "astrocommunity.colorscheme.nord-nvim" },
+
+  -- language packages:
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.markdown" },
   -- import/override with your plugins folder
 }
