c0d3a66911eece8f84f596bc048fbc077012fe25
Niki Suckau <niki.suckau@stud.uni.goettingen.de>
2025-01-26 13:39:51 +0000

    clangd plugins

--- lua/community.lua
+++ lua/community.lua
@@ -12,12 +12,18 @@ return {
   { import = "astrocommunity.colorscheme.tokyonight-nvim" },
   { import = "astrocommunity.colorscheme.tokyodark-nvim" },
   { import = "astrocommunity.colorscheme.nord-nvim" },
-  -- plugins
+  -- writing plugins
   { import = "astrocommunity.markdown-and-latex.vimtex" },
+  { import = "astrocommunity.completion.cmp-spell" },
+  { import = "astrocommunity.completion.cmp-latex-symbols" },
+  -- code plugins
   { import = "astrocommunity.completion.copilot-lua" },
-  { import = "astrocommunity.note-taking.obsidian-nvim" },
+  { import = "astrocommunity.code-runner.compiler-nvim" },
+  { import = "astrocommunity.code-runner.executor-nvim" },
   -- language packages:
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.python" },
   -- import/override with your plugins folder
 }
