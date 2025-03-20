8c01c0be5fedbd84a5b89b241899e338b53ac03d
Niki Suckau <niki.suckau@stud.uni-goettingen.de>
2025-03-20 20:58:06 +0100

    Themes

--- lua/community.lua
+++ lua/community.lua
@@ -13,6 +13,14 @@ return {
   { import = "astrocommunity.colorscheme.tokyonight-nvim" },
   { import = "astrocommunity.colorscheme.tokyodark-nvim" },
   { import = "astrocommunity.colorscheme.nord-nvim" },
+  { import = "astrocommunity.colorscheme.aurora" },
+  { import = "astrocommunity.colorscheme.dracula-nvim" },
+  { import = "astrocommunity.colorscheme.gruvbox-nvim" },
+  { import = "astrocommunity.colorscheme.gruvbox-baby" },
+  { import = "astrocommunity.colorscheme.helix-nvim" },
+  { import = "astrocommunity.colorscheme.neosolarized-nvim" },
+  { import = "astrocommunity.colorscheme.night-owl-nvim" },
+  { import = "astrocommunity.colorscheme.solarized-osaka-nvim" },
   -- writing plugins
   { import = "astrocommunity.markdown-and-latex.vimtex" },
   { import = "astrocommunity.completion.cmp-spell" },
@@ -20,13 +28,16 @@ return {
   -- code plugins
   -- { import = "astrocommunity.completion.copilot-lua" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
-  { import = "astrocommunity.code-runner.compiler-nvim" },
-  { import = "astrocommunity.code-runner.executor-nvim" },
+  -- { import = "astrocommunity.code-runner.compiler-nvim" },
+  -- { import = "astrocommunity.code-runner.executor-nvim" },
+  -- { import = "astrocommunity.code-runner.toggletasks-nvim" },
+  { import = "astrocommunity.code-runner.overseer-nvim" },
   -- language packages:
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.remote-development.remote-sshfs-nvim" },
+
   -- import/override with your plugins folder
 }
